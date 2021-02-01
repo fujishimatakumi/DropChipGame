9adb062ec5f4a3611325397bd7155cabe3d95cfd
motegi45 <uta.penpiece@docomo.ne.jp>
2021-02-01 11:45:22 +0900

    テキストの追加

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+// Photon 用の名前空間を参照する
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
 
 public class KillZone : MonoBehaviour
 {
+    [SerializeField] Text m_judgeText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("接触");
+        int number = PhotonNetwork.PlayerList.Length;
+        m_judgeText.text = number + "Win"; 
     }
 }
